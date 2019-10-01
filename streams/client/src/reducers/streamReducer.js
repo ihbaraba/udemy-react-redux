757cc667126df48ce8aed225168a173e0301fa22
ihbaraba <ihbaraba@gmail.com>
2019-10-01 19:02:32 +0300

    REST

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,15 @@
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case:
+        default:
+            return state;
+    }
+}
